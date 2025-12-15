f8c137ad24a9570f788e3983d547da53067c9d78
LBarros360 <lbarros360@gmail.com>
2025-12-15 19:26:26 -0300

    Add CPFException class for custom exception handling

---
+++ 0x06/api-rest-userController/demo/src/main/java/com/example/demo/exception/CPFException.java
@@ -0,0 +1,16 @@
+package com.example.demo.exception;
+
+public class CPFException extends RuntimeException {
+
+    public CPFException() {
+        super();
+    }
+
+    public CPFException(String message) {
+        super(message);
+    }
+
+    public CPFException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
